9c991cae9b4549dbfc16da0c7a3a736fa10df487
zaljifri <zainab.aljifri@gmail.com>
2022-11-08 22:24:58 +0300

    Update home screen

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:lets_chat/constants.dart';
 
 User? loggedInUser = FirebaseAuth.instance.currentUser;
 
@@ -14,31 +15,101 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
-
   String massageText = "";
   TextEditingController messageController = TextEditingController();
+
+  List<ChatMessage> messages = [
+    ChatMessage(
+        messageContent:
+            "Hello, Will jcfwe woifjw ioweo iowedj oidjweod weudhowe oiwdhedod owei",
+        messageType: "receiver"),
+    ChatMessage(messageContent: "How have you been?", messageType: "receiver"),
+    ChatMessage(
+        messageContent: "Hey Kriss, I am doing fine dude. wbu?",
+        messageType: "sender"),
+    ChatMessage(messageContent: "ehhhh, doing OK.", messageType: "receiver"),
+    ChatMessage(
+        messageContent: "Is there any thing wrong?", messageType: "sender"),
+    ChatMessage(messageContent: "Hello, Will", messageType: "receiver"),
+    ChatMessage(messageContent: "How have you been?", messageType: "receiver"),
+    ChatMessage(
+        messageContent: "Hey Kriss, I am doing fine dude. wbu?",
+        messageType: "sender"),
+    ChatMessage(messageContent: "ehhhh, doing OK.", messageType: "receiver"),
+    ChatMessage(
+        messageContent: "Is there any thing wrong?", messageType: "sender"),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: const Text('Chat')),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.end,
-          children: [
-            Container(
-              // decoration: kMessageContainerDecoration,
+      appBar: AppBar(
+        title: const Text('Chat'),
+        // backgroundColor: Colors.red,
+      ),
+      body: Stack(
+        children: <Widget>[
+          ListView.builder(
+            reverse: true,
+            itemCount: messages.length,
+            shrinkWrap: true,
+            padding: const EdgeInsets.only(top: 10, bottom: 70),
+            itemBuilder: (context, index) {
+              return Container(
+                padding: const EdgeInsets.only(
+                    left: 14, right: 14, top: 10, bottom: 10),
+                child: Align(
+                  alignment: (messages[index].messageType == "receiver"
+                      ? Alignment.topLeft
+                      : Alignment.topRight),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: (messages[index].messageType == "receiver"
+                          ? Colors.black26
+                          : Colors.teal),
+                    ),
+                    padding: EdgeInsets.all(16),
+                    child: Text(
+                      messages[index].messageContent,
+                      style: TextStyle(fontSize: 15),
+                    ),
+                  ),
+                ),
+              );
+            },
+          ),
+          Align(
+            alignment: Alignment.bottomLeft,
+            child: Container(
+              padding: const EdgeInsets.only(left: 10, bottom: 10, top: 10),
+              height: 60,
+              width: double.infinity,
+              color: kBGInput,
               child: Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
                 children: <Widget>[
                   Expanded(
                     child: TextField(
+                      textAlignVertical: TextAlignVertical.bottom,
                       controller: messageController,
                       onChanged: (value) {
                         massageText = value;
                       },
-                      // decoration: kMessageTextFieldDecoration,
+                      decoration: InputDecoration(
+                        hintText: "Write message...",
+                        border: InputBorder.none,
+                        filled: true,
+                        fillColor: Colors.black,
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: kBorderRadius,
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: kBorderRadius,
+                        ),
+                      ),
                     ),
                   ),
-                  TextButton(
+                  FloatingActionButton(
                     onPressed: () {
                       messageController.clear();
                       if (massageText!.isNotEmpty) {
@@ -48,15 +119,26 @@ class _HomeState extends State<Home> {
                         });
                       }
                     },
-                    child: const Text(
-                      'Send',
-                      // style: kSendButtonTextStyle,
+                    backgroundColor: Colors.blue,
+                    elevation: 0,
+                    child: const Icon(
+                      Icons.send,
+                      color: Colors.white,
+                      size: 18,
                     ),
                   ),
                 ],
               ),
             ),
-          ],
-        ));
+          ),
+        ],
+      ),
+    );
   }
+}
+
+class ChatMessage {
+  String messageContent;
+  String messageType;
+  ChatMessage({required this.messageContent, required this.messageType});
 }
